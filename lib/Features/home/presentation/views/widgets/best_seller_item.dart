5c7abe4b9b1e3f71f77798417ef870a86fef911d
Mo_Nasser <100493763+MoNasser155@users.noreply.github.com>
2024-01-30 20:52:27 +0200

    best seller item p2

--- lib/Features/home/presentation/views/widgets/best_seller_item.dart
+++ lib/Features/home/presentation/views/widgets/best_seller_item.dart
@@ -1,4 +1,6 @@
+import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/assets.dart';
+import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class BestSellerItem extends StatelessWidget {
@@ -27,8 +29,46 @@ class BestSellerItem extends StatelessWidget {
                 ),
               ),
             ),
-            const Column(
-              children: [],
+            const SizedBox(
+              width: 30,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  child: Text(
+                    'Harry Potter and the Gobel of fire',
+                    style: Styles.textStyle20.copyWith(
+                      fontFamily: kGtSektraFine,
+                    ),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                const SizedBox(
+                  height: 3,
+                ),
+                const Text(
+                  'J.K. Rowling',
+                  style: Styles.textStyle14,
+                ),
+                const SizedBox(
+                  height: 3,
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  child: Row(
+                    children: [
+                      Text(
+                        '19.99 \$',
+                        style: Styles.textStyle20
+                            .copyWith(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                )
+              ],
             ),
           ],
         ),
